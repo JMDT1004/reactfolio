436c533d12e71c36b115a3c01d139d234f7b8fd6
JT <raikiri24@gmail.com>
2023-08-08 22:43:46 -0400

    set up routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import Header from './components/Header';
 import Project from './components/Project';
 import Footer from './components/Footer';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
       <Header />
       <Project />
       <Footer />
+
+      <Routes>
+        <Route path='/' element='' />
+        <Route path='/about' element='' />
+        <Route path='/portfolio' element='' />
+        <Route path='/resume' element='' />
+        <Route path='/contact' element='' />
+      </Routes>
 
     </div>
   );
